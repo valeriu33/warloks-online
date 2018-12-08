15d03c2a8cce7641d66d2f876e9c5decca575369
Emil Terman <terman.emil@gmail.com>
2018-12-09 01:31:00 +0200

    Finished smooooooth movement

--- Warloks/Assets/PlayerMovement.cs
+++ Warloks/Assets/PlayerMovement.cs
@@ -4,37 +4,69 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float speed = 1F;
+    [SerializeField] private float speed = 1f;
+
+    [Header("Amortization")]
+    [SerializeField] private float amortizationTreshold = 10f;
+    [SerializeField] private float amortizationSpeedMult = 0.4f;
+
     private Vector3 velocity = Vector3.zero;
     private Vector3 targetPosition;
+    private Vector3 mousePos;
 
-    // Use this for initialization
-    void Start ()
-	{
-		
-	}
-	
-	// Update is called once per frame
 	void Update ()
 	{
-	    var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	    mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButton(0))
 	    {
-	        targetPosition = mousePosition;
+	        targetPosition = mousePos;
 	        targetPosition.z = 0;
-	        //            gameObject.transform.position = pz;
-
 	    }
-
-        Vector3 diff = mousePosition - transform.position;
-	    diff.Normalize();
-
-	    float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-	    transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
-
-	    transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, 1/speed);
-
-        
-
 	}
+
+    private void FixedUpdate()
+    {
+        RotateToPos(mousePos);
+        MoveTowardsPos(targetPosition, Time.fixedDeltaTime);
+    }
+
+    private void RotateToPos(Vector3 pos)
+    {
+        Vector3 diff = pos - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+    }
+
+    private void MoveTowardsPos(Vector3 pos, float deltaTime)
+    {
+        var delta = pos - transform.position;
+        delta.z = 0;
+
+        if (Mathf.Approximately(delta.magnitude, 0))
+            return;
+
+        if (delta.magnitude < amortizationTreshold)
+        {
+            transform.position = Vector3.SmoothDamp(
+                transform.position,
+                targetPosition,
+                ref velocity,
+                1 / (amortizationSpeedMult * speed));
+        }
+        else
+        {
+            var distance = speed * deltaTime;
+            var finalDistance = 0f;
+
+            if (delta.magnitude < distance)
+                finalDistance = delta.magnitude;
+            else
+                finalDistance = distance;
+
+            var direction = delta.normalized;
+            transform.position = transform.position + direction * finalDistance;
+        }
+    }
 }
