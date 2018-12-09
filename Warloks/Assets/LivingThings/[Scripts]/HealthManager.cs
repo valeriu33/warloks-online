8208b8008b92b2bcbb64d723edc39b5a051c9f1b
Emil Terman <terman.emil@gmail.com>
2018-12-09 17:24:24 +0200

    Rearranged some stuff and added a simple Health bar

---
+++ Warloks/Assets/LivingThings/[Scripts]/HealthManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] private float maxHp = 100;
+    public float MaxHp { get { return maxHp; } }
+
+    [SerializeField] private Image hpBar = null;
+    [SerializeField] private UnityEvent onDie = null;
+
+    public float Health { get; private set; }
+
+    private void Start()
+    {
+        SetHealth(maxHp);
+    }
+
+    private void Update()
+    {
+        if (hpBar != null)
+        {
+            hpBar.transform.rotation = Quaternion.Euler(Vector3.zero);
+        }
+    }
+
+    public float TakeDmg(float dmg)
+    {
+        var initialHp = Health;
+
+        SetHealth(Health - dmg);
+        if (Health <= 0)
+            Die();
+
+        return initialHp - Health;
+    }
+
+    public void SetHealth(float health)
+    {
+        Health = Mathf.Clamp(health, 0, maxHp);
+        UpdateHpBar();
+    }
+
+    public void Die()
+    {
+        onDie.Invoke();
+    }
+
+    private void UpdateHpBar()
+    {
+        if (hpBar == null)
+            return;
+
+        hpBar.fillAmount = Health / maxHp;
+    }
+}
